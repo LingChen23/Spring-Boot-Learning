5ad62d3c6c226cf8bf5671b582856b2ad48cd49c
yy <1667253917@qq.com>
2024-07-02 21:54:43 +0800

    前端路由VueRouter

---
+++ router-demo/src/components/My.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>我的音乐</h1>
+        <router-link to="/my/1">商品1</router-link>
+        <router-link to="/my/2">商品2</router-link>
+        <router-link to="/my/3">商品3</router-link>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ router-demo/src/components/Product.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        props: ["id"]
+    }
+</script>
+
+<template>
+<h3>商品{{id}}</h3>
+</template>
+
+<style scoped>
+
+</style>
