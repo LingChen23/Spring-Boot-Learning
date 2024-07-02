765d20e7c9493a732361c98c732120765e3d146e
yy <1667253917@qq.com>
2024-07-02 23:42:24 +0800

    状态管理VueX

---
+++ vuex-demo/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+<!--    {{this.$store.state.count}}-->
+      {{count}}
+      <button @click="add">+1</button>
+
+      <ul>
+          <li v-for="todo in doneTools" :key="todo.id">{{todo.text}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from "vuex";
+
+//映射 this.count 为 store.state.count
+// 'count': state => state.count
+
+export default {
+  name: 'HelloWorld',
+    computed: {
+      ...mapState([
+          'count', 'todos'
+      ]),
+
+      ...mapGetters([
+          "doneTools"
+      ])
+    },
+    methods:{
+      add(){
+          this.$store.commit("increment",
+              3);
+      }
+    }
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ vuex-demo/src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+        todos: [
+            { id: 1, text: '吃饭', done: true },
+            { id: 2, text: '睡觉', done: false },
+        ]
+    },
+    mutations: {
+        increment(state, n) {
+            state.count += n
+        }
+    },
+    getters: {
+        doneTools: state => {
+            return state.todos.filter(todo => todo.done)
+        }
+    }
+})
+
+export default store
